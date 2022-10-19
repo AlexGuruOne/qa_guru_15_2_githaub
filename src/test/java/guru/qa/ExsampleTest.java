f67399d96d912853f3bebc546daf63bf22e402f8
AlexGuruOne <aleksandrsemiklet@gmail.com>
2022-10-19 12:12:50 +0600

    init commit from console

---
+++ src/test/java/guru/qa/ExsampleTest.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExsampleTest {
+
+    @Test
+    void exsampletest() {
+        Assertions.assertTrue(2 > 1);
+    }
+}
