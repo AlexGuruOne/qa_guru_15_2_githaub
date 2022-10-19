515f4e535deb22006b0401b5bb53f77ce2b8440b
AlexGuruOne <aleksandrsemiklet@gmail.com>
2022-10-19 13:13:00 +0600

    fix logic

--- src/test/java/guru/qa/ExsampleTest.java
+++ src/test/java/guru/qa/ExsampleTest.java
@@ -6,7 +6,14 @@ import org.junit.jupiter.api.Test;
 public class ExsampleTest {
 
     @Test
-    void exsampletest() {
-        Assertions.assertTrue(2 > 1);
+    void exsampletest0() {
+
+        Assertions.assertTrue(2 >= 1);
+    }
+
+    @Test
+    void exsampletest1() {
+
+        Assertions.assertTrue(3 >= 2);
     }
 }
