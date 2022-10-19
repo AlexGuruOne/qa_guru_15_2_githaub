1798b42a69c7454f26bf94917985dc5aca7ee75a
AlexGuruOne <aleksandrsemiklet@gmail.com>
2022-10-19 14:28:59 +0600

    Added test2

--- src/test/java/guru/qa/ExsampleTest.java
+++ src/test/java/guru/qa/ExsampleTest.java
@@ -3,15 +3,17 @@ package guru.qa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExsampleTest {
 
     @Test
     void exsampletest0() {
-        Assertions.assertTrue(7 >= 10);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
     void exsampletest1() {
-        Assertions.assertTrue(7 >= 10);
+        Assertions.assertTrue(new Random().nextInt() > 2);
     }
 }
